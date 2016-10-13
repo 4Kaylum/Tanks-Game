18040af8054b49f6dc752d4acf1ea9e5890ec302
Callum Bartlett <coolum444@hotmail.co.uk>
2016-10-13 18:28:01 +0100

    Start on rotation, somewhat fix collisions

--- playerClass.py
+++ playerClass.py
@@ -2,6 +2,7 @@ import pygame
 from bulletClass import *
 from time import time
 import math
+from gameConstants import *
 
 # Create the player class
 class Player(pygame.sprite.Sprite):
@@ -19,8 +20,8 @@ class Player(pygame.sprite.Sprite):
 
         # Set change variables - these will be checked before moving the player to make sure
         # that there are no collisions
-        self.X_c = 0
-        self.Y_c = 0
+        self.X_c = 0.0
+        self.Y_c = 0.0
 
         # Store playerNumber so it doesn't need to be read again later
         self.playerNumber = playerNumber
@@ -36,7 +37,7 @@ class Player(pygame.sprite.Sprite):
         self.bullet = None
 
         # Store it's last moved direction. Up left down right, 0 1 2 3
-        self.direction = 0
+        self.direction = 0.0
         self.rotation = 0 
 
         # Make it so you can't spam bullets
@@ -50,10 +51,10 @@ class Player(pygame.sprite.Sprite):
     # Moves a player's relative location
     def moveLocation(self):
 
-        self.rect.x += self.X_c
-        self.rect.y += self.Y_c
-        self.X_c = 0
-        self.Y_c = 0
+        self.rect.x += int(self.X_c)
+        self.rect.y += int(self.Y_c)
+        # self.X_c = 0
+        # self.Y_c = 0
 
     # Load the settings from the file
     def readSettings(self, jsonFile):
@@ -73,54 +74,87 @@ class Player(pygame.sprite.Sprite):
     def checkKeypress(self):
         x = pygame.key.get_pressed()
 
-        ## Change rotation value based on LR values
+        # Change rotation value based on LR values
         if x[self.buttons['right']]:
-            self.X_c += 3 # Subject to change
-            self.rotation -= 1
-        if x[self.buttons['left']]:
-            self.X_c -= 3 # Subject to change
-            self.rotation += 1
+            # self.X_c += playerMovementAmount
+            self.rotation -= playerRotationAmount
+        elif x[self.buttons['left']]:
+            # self.X_c -= playerMovementAmount
+            self.rotation += playerRotationAmount
+        else:
+            self.X_c = 0
+            self.rotation = 0
 
-        ## Change XY values based on UD values
+        # Change XY values based on UD values
         if x[self.buttons['up']]:
-            self.Y_c -= 3 # Subject to change
-        if x[self.buttons['down']]:
-            self.Y_c += 3 # Subject to change
+            # self.Y_c -= playerMovementAmount
+            self.anglePosChange(True)
+        elif x[self.buttons['down']]:
+            # self.Y_c += playerMovementAmount
+            self.anglePosChange(False)
+        else:
+            self.Y_c = 0
 
-        if self.rotation > 360:
+        if self.rotation >= 360:
             self.rotation -= 360
         elif self.rotation < 0:
             self.rotation += 360
 
-        if self.parent.frame > self.lastShot + 20:
+        if self.parent.frame > self.lastShot + bulletFrameTimeout:
             self.lastShot = self.parent.frame
             if x[self.buttons['fire']]:
                 self.bullet = Bullet(self)
 
-    def rotCentre(self, image, rect, angle):
-        """rotate an image while keeping its center"""
-        rot_image = pygame.transform.rotate(image, angle)
-        rot_rect = rot_image.get_rect(center=rect.center)
-        return rot_image,rot_rect
+    # Get X/Y change values from an angle
+    def anglePosChange(self,forward=True):
+        r = self.rotation
+
+        self.X_c = math.sin(r) * playerMovementAmount
+
+        # decreasing x decreasing y
+        if r >= 0 and r < 90:
+            pass
+
+        # decreasing x increasing y
+        elif r >= 90 and r < 180:
+            pass
+            
+        # increasing x increasing y
+        elif r >=180 and r < 270:
+            pass
+
+        # increasing x decreasing y
+        elif r >= 270 and r < 360:
+            pass
+
+
+    def rotCentre(self):
+        rot_image = pygame.transform.rotate(self.image, self.angle)
+        rot_rect = rot_image.get_rect(center=self.rect.center)
+        return rot_image, rot_rect
 
     # Check any collisions between itself and another object
     def checkCollide(self, wallGroup):
+        # Move the object to make sure it's collided
+        self.moveLocation()
         # Check what it's hit with - gives list of images
         hitList = pygame.sprite.spritecollide(self, wallGroup, False)
         # Go through each one, find it's bound
         for block in hitList:
             # Check top-bottom
             if self.Y_c < 0: # If going up set top to block's bottom
-                self.rect.top = block.rect.bottom + 3
+                self.rect.top = block.rect.bottom
             elif self.Y_c > 0:
-                self.rect.bottom = block.rect.top - 3
+                self.rect.bottom = block.rect.top
 
         # Regenerate list to avoid conflits
         hitList = pygame.sprite.spritecollide(self, wallGroup, False)
         for block in hitList:
             # Check left-right
             if self.X_c < 0:
-                self.rect.left = block.rect.right + 3
+                self.rect.left = block.rect.right
             elif self.X_c > 0:
-                self.rect.right = block.rect.left - 3
-        self.moveLocation()
+                self.rect.right = block.rect.left
+
+        self.X_c = self.Y_c = 0
+        # self.moveLocation()
