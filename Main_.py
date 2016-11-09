4c5d37333fb79ecb59a80bfe4b0984fc6ad75ac7
Win_PCLaptop Callum Bartlett <coolum444@hotmail.co.uk>
2016-11-09 10:34:39 +0000

    Different window states moved to own module

--- Main_.py
+++ Main_.py
@@ -1,11 +1,8 @@
 # Import the main library to use for the game
 import pygame
 # Import self-made classes
-from wallClass import *
-from playerClass import *
 from windowClass import *
-from bulletClass import *
-from gameConstants import *
+import windowStates
 
 
 # Main running code
@@ -32,54 +29,14 @@ if __name__ == '__main__':
         # Increment frame count
         window.frame += 1
 
-        # Clean the map
-        if window.windowState not in [1, 4]:
-            window.clean()
-            window.wallGroup = pygame.sprite.Group()
-
         # If playing or generating the level
         if window.windowState in [1, 4]:
-            # Set level up
-            if window.windowState == 4:
-                window.windowState = 1
+            windowStates.playLevel(window)
 
-                # Load the level
-                if loadLevel != '':
-                    window.setLevel(levelName=loadLevel,randomLevel=False)
-                else:
-                    window.setLevel(randomLevel=True)
-
-                # Generate the level
-                window.makeWalls(window.levelPath())
-                window.playerStartupLocations(window.levelPath())
-
-
-            # Do all the movement and collision and stuff
-            window.do()
-
-            # Debug strings~
-            frameString = str(hex(window.frame)).upper()[2:]
-            print('{} {} {}'.format(frameString, window.playerOne, window.playerTwo))
 
         # If titlescreen
         elif window.windowState == 0:
-            # Create the clickbox
-            window.wallGroup.add(Wall(topLeft=[50, 50], dimensions=[300, 50], colour=[0,0,0], name='PlayButton'))
-
-            # Update and draw
-            window.drawAll(drawPlayerData=False, drawPlayers=False, clear=False, update=False)
-
-            # Draw the text on top of it
-            window.makeFont('Play', [200,75], colour=[255, 255, 255])
-
-            # Update display
-            pygame.display.flip()
-
-            # Check if it was clicked
-            if pygame.MOUSEBUTTONDOWN in [i.type for i in window.events]:
-                ifClicked = [o.checkClick(pygame.mouse.get_pos()) for o in window.wallGroup][0]
-                if ifClicked:
-                    window.windowState = 4
+            windowStates.titleScreen(window)
 
 
 
---
+++ windowStates.py
@@ -0,0 +1,48 @@
+from wallClass import *
+from windowClass import *
+
+def playLevel(window):
+    # Set level up
+    if window.windowState == 4:
+        window.windowState = 1
+
+        # Load the level
+        if loadLevel != '':
+            window.setLevel(levelName=loadLevel,randomLevel=False)
+        else:
+            window.setLevel(randomLevel=True)
+
+        # Generate the level
+        window.makeWalls(window.levelPath())
+        window.playerStartupLocations(window.levelPath())
+
+
+    # Do all the movement and collision and stuff
+    window.do()
+
+    # Debug strings~
+    frameString = str(hex(window.frame)).upper()[2:]
+    print('{} {} {}'.format(frameString, window.playerOne, window.playerTwo))
+
+def titleScreen(window):
+    # Clear the window
+    window.clean()
+    window.wallGroup = pygame.sprite.Group()
+    
+    # Create the clickbox
+    window.wallGroup.add(Wall(topLeft=[50, 50], dimensions=[300, 50], colour=[0,0,0], name='PlayButton'))
+
+    # Update and draw
+    window.drawAll(drawPlayerData=False, drawPlayers=False, clear=False, update=False)
+
+    # Draw the text on top of it
+    window.makeFont('Play', [200,75], colour=[255, 255, 255])
+
+    # Update display
+    pygame.display.flip()
+
+    # Check if it was clicked
+    if pygame.MOUSEBUTTONDOWN in [i.type for i in window.events]:
+        ifClicked = [o.checkClick(pygame.mouse.get_pos()) for o in window.wallGroup][0]
+        if ifClicked:
+            window.windowState = 4
